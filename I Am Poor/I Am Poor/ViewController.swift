19ba6838449bb288aae42b8de2ad0c18f5779d23
Mathe-S <sharvadzemat@gmail.com>
2021-09-06 17:03:24 +0400

    static apps

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Mathe Sharvadze on 06.09.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
